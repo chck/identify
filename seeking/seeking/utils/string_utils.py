1c2d126309052e0fb149b8ccd2b7dbcd141336b2
chck <deadline.is.today@gmail.com>
2018-09-23 03:36:03 +0900

    Add utility for string

--- seeking/seeking/utils/string_utils.py
+++ seeking/seeking/utils/string_utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 from os import path
 
 
@@ -12,3 +13,10 @@ def get_filename(builtin_file: str) -> str:
 
 def to_camel(snake: str) -> str:
     return snake.title().replace('_', '')
+
+
+def to_snake(camel: str) -> str:
+    """https://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case
+    """
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', camel)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
